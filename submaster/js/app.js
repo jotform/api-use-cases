bfd6a91bf28c7957e7c25ecd646ed89c7ad0cb6a
mustafa dikici <mus.dikici@gmail.com>
2013-07-09 10:35:02 +0300

    code cleaning, refactoring and working to setup router to launch app with :formID

--- submaster/js/app.js
+++ submaster/js/app.js
@@ -12,102 +12,103 @@ $(document).ready(function(){
 
     var self = window.app = this;
 
-    function initializeApp(userModelObject){
+    function initializeApp(){
 
-
-        JF.getUser(function(user){
-
-            $("#username").html(user.name);
-            $("#avatar").attr("src", user.avatarUrl);
-            JF.getForms(function(forms){
-                $(".form-list").show();
-                
-                if (typeof user.avatarURL !== 'undefined') {
-                    user.avatarUrl = user.avatarURL;
-                } else if (typeof user.avatarUrl !== 'undefined') {
-                    user.avatarURL = user.avatarUrl;
-                }
-                window.app.user = user;
-                window.app.formsCollection = new FormsCollection(forms);
-                window.app.submissionsCollection = new SubmissionsCollection();
-                
-                window.app.sidebarView = new SidebarView();
-
-                window.app.homeView = new HomeView();
-
-                window.app.cache = {};
-                // window.app.mainTabView = new MainTabView();
-                
-                var formNames = [];
-                for(var i=0; i<forms.length; i++){
-                    formNames.push(forms[i].title);
-                }
-
-                var datums = [];
-                for(var i=0; i<forms.length; i++){
-                    datums.push({
-                        value: forms[i].title,
-                        tokens: [forms[i].title], 
-                        id: forms[i].id
-                    });
-                }                
-
-                var t = $(".form-list .typeahead").typeahead({
-                    name: "forms",
-                    local: datums,
-                    template: '<p><strong>{{value}}</strong></p>',
-                    engine: Hogan
-                });
-
-                t.on("typeahead:selected", function(evt, data){
-                    app.sidebarView.addTab(data);
-                });
-
-
-
-            });
-
-        });
-
-
-        $(".login-button").click(function(){
-            // JF.login(function(){
-            //     console.log("user logged in");
-            //     JF.getUser(function(r){
-            //         console.log("user is ", r);
-            //     })
-            // })
-        });
-        
         //define router
         var Router = Backbone.Router.extend({
-            // routes: {
-            //     "": "showProfile",
-            //     "profile": "showProfile",
-            //     "users": "showUsers",
-            //     "settings": "showSettings",
-            //     "billing": "showBilling",
-            //     "api": "showApi",
-            //     "history": "showHistory",
-            //     "usage": "showUsage",
-            //     "getMore": "showGetMore"
-            // },
+            routes: {
+                "": "initializeHomeView",
+                ":formID": "initializeFormView"
+            },
+            initializeHomeView : function(){
+                JF.getUser(function(user){
+                    $("#username").html(user.name);
+                    $("#avatar").attr("src", user.avatarUrl);
+                    JF.getForms(function(forms){
+                        //typeahead text box
+                        $(".form-list").show();
+                        
+                        if (typeof user.avatarURL !== 'undefined') {
+                            user.avatarUrl = user.avatarURL;
+                        } else if (typeof user.avatarUrl !== 'undefined') {
+                            user.avatarURL = user.avatarUrl;
+                        }
 
-            // showProfile :   function(){ self.stateModel.set("currentTabId", "profile");  if(window.app.needRefresh){ window.location.reload(); } },
-            // showUsers:      function(){ self.stateModel.set("currentTabId", "users"); if(window.app.needRefresh){ window.location.reload(); } },
-            // showSettings:   function(){ self.stateModel.set("currentTabId", "settings"); if(window.app.needRefresh){ window.location.reload(); } },
-            // showBilling:    function(){ self.stateModel.set("currentTabId", "billing"); if(window.app.needRefresh){ window.location.reload(); } },
-            // showApi:        function(){ self.stateModel.set("currentTabId", "api"); if(window.app.needRefresh){ window.location.reload(); } },
-            // showHistory:    function(){ self.stateModel.set("currentTabId", "history"); if(window.app.needRefresh){ window.location.reload(); } },
-            // showUsage:      function(){ self.stateModel.set("currentTabId", "usage"); if(window.app.needRefresh){ window.location.reload(); } },
-            // showGetMore:    function(){ self.stateModel.set("currentTabId", "getMore"); if(window.app.needRefresh){ window.location.reload(); } }
+                        window.app.user = user;
+                        window.app.formsCollection = new FormsCollection(forms);
+                        window.app.submissionsCollection = new SubmissionsCollection();
+                        
+                        window.app.sidebarView = new SidebarView();
+
+                        window.app.homeView = new HomeView();
+
+                        
+                        var formNames = [];
+                        for(var i=0; i<forms.length; i++){
+                            formNames.push(forms[i].title);
+                        }
+                        createTypeAhead(forms);
+                    });
+                });
+            },
+            initializeFormView : function(formID){
+                JF.getUser(function(user){
+                    $("#username").html(user.name);
+                    $("#avatar").attr("src", user.avatarUrl);
+                    JF.getForms(function(forms){
+                        //typeahead text box
+                        $(".form-list").show();
+                        if (typeof user.avatarURL !== 'undefined') {
+                            user.avatarUrl = user.avatarURL;
+                        } else if (typeof user.avatarUrl !== 'undefined') {
+                            user.avatarURL = user.avatarUrl;
+                        }
+
+                        window.app.user = user;
+                        window.app.formsCollection = new FormsCollection(forms);
+                        window.app.submissionsCollection = new SubmissionsCollection();
+                        
+                        window.app.sidebarView = new SidebarView();
+
+                        window.app.homeView = new HomeView();
+
+                        var formNames = [];
+                        for(var i=0; i<forms.length; i++){
+                            formNames.push(forms[i].title);
+                        }
+
+                        createTypeAhead(forms);
+                    });
+                });                
+            }
         });
-        //and initialize
-        // self.router = new Router();
-        // Backbone.history.start({pushState: true, root: "/myaccount/"});
+
+        //initialize router
+        self.router = new Router();
+        Backbone.history.start({pushState: false});
 
 
     };
+
+    function createTypeAhead(forms) {
+        var datums = [];
+        for(var i=0; i<forms.length; i++){
+            datums.push({
+                value: forms[i].title,
+                tokens: [forms[i].title], 
+                id: forms[i].id
+            });
+        }                
+        var t = $(".form-list .typeahead").typeahead({
+            name: "forms",
+            local: datums,
+            template: '<p><strong>{{value}}</strong></p>',
+            engine: Hogan
+        });
+        t.on("typeahead:selected", function(evt, data){
+            app.sidebarView.addTab(data);
+        });        
+    }    
 
     if( JF.getAPIKey() === null){
         JF.login(function(){
@@ -116,28 +117,7 @@ $(document).ready(function(){
     } else {
         initializeApp();
     }
-    //because it is asynchronous we need to initialize main view after successfull response of user object from API
-    //also this way we show nothing to loggedOut users
-    
-    // JotformAPI.getUserDetail({
 
-    //     success : function(res){
-    //         if (res.message == "success"){
-    //             var userDetails = res.content;
-    //             initializeApp(userDetails);
-    //         }
-    //     },
-
-    //     error: function(){
-
-    //         loginModalBoxBuilder({
-    //             onSuccessfullLogin: function(res){
-    //                 initializeApp(res.content);
-    //             }
-    //         });
-
-    //     }
-    // });
 });
 
 

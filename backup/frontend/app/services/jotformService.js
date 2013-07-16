26f92f66158927eaf6abc5eaeb264be7a209ef70
Kemal Dağ <gkemaldag@gmail.com>
2013-07-16 16:55:17 +0300

    jotform backup: tasks array constructed to be sent to backend

--- backup/frontend/app/services/jotformService.js
+++ backup/frontend/app/services/jotformService.js
@@ -13,7 +13,40 @@ jotModule.factory('jotservice',function($q,$timeout,$http){
 	};
 
 	jotservice.sendFormListToServer = function(forms){
-		return $http.post("/goback/addBackupTasks",forms).then(function(response){
+
+		//prepare post parameters, form list should be form id array of all forms
+		// submissions tasks should be an array of 50 submissions at once,
+		// for example if form x has 120 submissions then submission backup tasks should be [{range:[0,50]},{range:[51,100],[range:101,120]}]
+		var tasks = [];
+		var submissions_per_task = 50; 
+		for(var i=0; i<forms.length;i++){
+			var curform = forms[i];
+			var curTask = {
+				formTitle : curform.title,
+				id : curform.id,
+				submissionTasks : []
+			}
+
+			//populate submissionsTasks array
+			if(curform.count != 0){ //add tasks only if count bigger than zero
+
+				var sub_tasks_count = Math.ceil(parseInt(curform.count)/submissions_per_task);
+				var low_range = 1;
+				var high_range = submissions_per_task; //this values will be 0,50 at first run
+				for(var j=0; j<sub_tasks_count;j++){
+					curTask.submissionTasks.push([low_range,high_range]);
+					low_range = high_range+1;
+					high_range = high_range + submissions_per_task; //modify low range high range for next loop execution
+				}
+			}
+
+		tasks.push(curTask); //push prepared task to tasks array   
+
+
+		}
+		console.log(tasks);
+
+		return $http.post("/goback/addBackupTasks",{tasks:angular.toJson(tasks)}).then(function(response){
 			console.log("/addBackupTasks  => ",response);
 			return response;
 		});

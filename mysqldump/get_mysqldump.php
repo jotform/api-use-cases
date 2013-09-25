2f61250343aef84e849accc14910dfd56544466f
ceren <ceren@jotform.com>
2013-09-25 18:47:59 +0300

    new DB dumo files included

--- mysqldump/get_mysqldump.php
+++ mysqldump/get_mysqldump.php
@@ -1,13 +1,32 @@
 <?
-
 	$apiKey = $_GET['apiKey'];
 	$formID = $_GET['formID'];
 	$format = $_GET['format'];
 
 	include 'jotform_mysqldump.php';
-	$sql = jotform_mysqldump( $apiKey, $formID, $format);
+	include 'jotform_postgresqldump.php';
+	include 'jotform_oracledump.php';
+	include 'jotform_sqlserverdump.php';
+
 	if( $formID < 1)
 		die("Form ID Missing!");
+
+	switch ($format) {
+		case "PostgreSQL":
+			$sql = jotform_postgresqldump( $apiKey, $formID, $format);
+			break;
+		case "Oracle":
+			$sql = jotform_oracledump( $apiKey, $formID, $format);
+			break;
+		case "SQL Server":
+			$sql = jotform_sqlserverdump( $apiKey, $formID, $format);
+			break;
+		default:
+			$sql = jotform_mysqldump( $apiKey, $formID, $format);
+			break;
+	}
+
+
 	chdir("zip/");
 	$sqlFile = "$formID.sql";
 	$zipFile = "sql$formID.zip";
@@ -24,7 +43,5 @@
     print $url;
 
 	//print "<pre>$sql";
-
-
 
 ?>
